17d9f67d39539094ad96822c8ef4426fd052d9ed
clubberstake <Gregory.Lang@gmail.com>
2016-03-29 20:44:57 -0400

    Adding delete account controller

---
+++ AlphaSchoolWeb/Controllers/DeleteAccountController.cs
@@ -0,0 +1,46 @@
+﻿using AlphaSchoolWeb.Model;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlphaSchoolWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class DeleteAccountController : Controller
+    {
+        [HttpGet]
+        public string DeleteAccount()
+        {
+            var studentList = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Vicky",
+                    LastName = "Danko",
+                    ID = "1"
+                },
+                new Student()
+                {
+                    FirstName = "Abdul",
+                    LastName = "Fall",
+                    ID = "2"
+                },
+                new Student()
+                {
+                    FirstName = "Bret",
+                    LastName = "Pusateri",
+                    ID = "3"
+                }
+            };
+            return JsonConvert.SerializeObject(studentList);
+        }
+
+        [HttpDelete]
+        public string DeleteAccount(string id)
+        {
+            return "Account Deleted Passed ->" + id;
+        }
+    }
+}
