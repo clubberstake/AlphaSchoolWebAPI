56e2eaa3fd436b3d6efafaef816927533e45d9d2
Vicky Danko <victoria.danko@stu.laroche.edu>
2016-04-05 23:13:29 +0000

    made changes to Admin routes

---
+++ AlphaSchoolWeb/Model/CourseNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaSchoolWeb.Model
+{
+    public static class CourseNames
+    {
+        public static List<string> Names = new List<string>()
+        {
+            "Science",
+            "Math",
+            "English",
+            "History"
+        };
+    }
+}
