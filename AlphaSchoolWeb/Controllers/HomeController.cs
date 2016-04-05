94ebbac4f822c13cd2b7790f4ed8c3156c9b5007
Bret Pusateri <bret.pusateri@stu.laroche.edu>
2016-04-05 23:20:53 +0000

    home controller

---
+++ AlphaSchoolWeb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AlphaSchoolWeb.Model;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlphaSchoolWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string ViewCourses()
+        {
+            var courseList = new List<Course>()
+            {
+                new Course()
+                {
+                    ID = "1",
+                    CourseName = "Math",
+                    Grade = 4
+                },
+
+                new Course()
+                {
+                    ID = "2",
+                    CourseName = "Science",
+                    Grade = 2
+                },
+
+                new Course()
+                {
+                   ID = "3",
+                   CourseName = "English",
+                   Grade = 3
+                }
+              
+            };
+            return JsonConvert.SerializeObject(courseList);
+        }
+
+    }
+}
