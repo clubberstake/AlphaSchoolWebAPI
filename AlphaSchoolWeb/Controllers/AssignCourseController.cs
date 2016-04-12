c6e467fe6928885b47058ea793ed4781e99029cb
AbdoFallatah <F1abdo@hotmail.com>
2016-04-12 18:40:35 -0400

    adding assign course controller

--- AlphaSchoolWeb/Controllers/AssignCourseController.cs
+++ AlphaSchoolWeb/Controllers/AssignCourseController.cs
@@ -1,4 +1,6 @@
 ﻿using AlphaSchoolWeb.Model;
+using AlphaSchoolWeb.Request;
+using AlphaSchoolWeb.Response;
 using Microsoft.AspNet.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -13,34 +15,48 @@ namespace AlphaSchoolWeb.Controllers
         [HttpGet]
         public string IdList()
         {
-            var IDList = new List<Course>()
-            {
-                new Course()
-                {
-                    ID = "101",
-                    CourseName = "Math",
-                    Grade = 1
-                },
-                new Course()
-                {
-                     ID = "201",
-                    CourseName = "Science",
-                    Grade = 1
-                },
-                new Course()
-                {
-                    ID = "301",
-                    CourseName = "English",
-                    Grade = 1
-                }
-            };
-            return JsonConvert.SerializeObject(IDList);
+            return "wrong";           
         }
 
         [HttpGet("{id}")]
         public string Get(string id)
         {
-            return "this is the ID:" + id;
+            var studentList = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Vicky",
+                    LastName = "Danko",
+                    ID = "1"
+                },
+                new Student()
+                {
+                    FirstName = "Abdul",
+                    LastName = "Fall",
+                    ID = "2"
+                },
+                new Student()
+                {
+                    FirstName = "Bret",
+                    LastName = "Pusateri",
+                    ID = "3"
+                }
+            };
+
+            var assignCourseResponse = new AssignCourseResponse();
+            assignCourseResponse.Students = studentList;
+            assignCourseResponse.CourseNames = CourseNames.Names;
+            assignCourseResponse.CourseLevels = CourseLevel.Levels;
+
+            return JsonConvert.SerializeObject(assignCourseResponse);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody] AssignCourseRequest assignCourseRequest)
+        {
+            return "Saving Id " + assignCourseRequest.Id + " course Id " + assignCourseRequest.CourseId +
+                " for grade " + assignCourseRequest.Grade + " for level " +assignCourseRequest.Level;
         }
     }
 }
---
+++ AlphaSchoolWeb/Request/AssignCourseRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaSchoolWeb.Request
+{
+    public class AssignCourseRequest
+    {
+        public string Id { get; set; }
+        public string CourseId { get; set; }
+        public string Grade { get; set; }
+        public string Level { get; set; }
+    }
+}
